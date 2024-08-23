d7d08b47b948b1f21e88e6943e26abe3a06a3f06
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-08-23 14:25:59 -0700

    [215] [Time Beats: 26.54%] [Memory Beats: 18.97%] - LeetPush

---
+++ 215-Kth-Largest-Element-in-an-Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minheap = []
+        for i in range(k):
+            minheap.append(nums[i])
+        heapq.heapify(minheap)
+        print(minheap)
+        for i in range(k, len(nums)):
+            if nums[i] > minheap[0]:
+                heapq.heappop(minheap)
+                heapq.heappush(minheap, nums[i])
+        return minheap[0]
