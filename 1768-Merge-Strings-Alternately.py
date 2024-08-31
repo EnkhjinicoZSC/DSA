68d4a111ed7991c963094765ed7588eb2173094b
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-08-31 00:09:15 -0700

    [1768] [Time Beats: 36.81%] [Memory Beats: 71.92%] - LeetPush

---
+++ 1768-Merge-Strings-Alternately.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        l1, l2 = len(word1), len(word2)
+        res = ""
+        if l1 == l2:
+            for i in range(l1):
+                res += word1[i] + word2[i]
+        elif l1 < l2:
+            for i in range(l1):
+                res += word1[i] + word2[i]
+            res += word2[l1:]
+        else:
+            for i in range(l2):
+                res += word1[i] + word2[i]
+            res += word1[l2:]
+        return res
