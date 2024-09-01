ad7e6ae14a64b97f2389e58cc7fade98dbf4060e
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-08-31 21:03:12 -0700

    [496] [Time Beats: 63.05%] [Memory Beats: 26.12%] - LeetPush

--- 496-Next-Greater-Element-I.py
+++ 496-Next-Greater-Element-I.py
@@ -1,15 +1,11 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = [-1] * len(nums1)
-        h = defaultdict(list)
-        for idx, num in enumerate(nums2):
-            h[num].append(idx)
-        print(h)
-        for i, num in enumerate(nums1):
-            if num in h:
-                idx = h[num][0]
-                for j in range(idx, len(nums2)):
-                    if nums2[j] > num:
-                        ans[i] = nums2[j]
-                        break
-        return ans
+        h = {}
+        stack = []
+        for num2 in nums2:
+            while stack and num2 > stack[-1]:
+                h[stack.pop()] = num2
+            stack.append(num2)
+        while stack:
+            h[stack.pop()] = -1
+        return [h.get(num, -1) for num in nums1]
