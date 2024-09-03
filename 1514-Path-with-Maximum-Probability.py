b42776254984905f34cb688e729e7215e73a76b5
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-09-03 13:53:54 -0700

    [1514] [Time Beats: 76.32%] [Memory Beats: 32.62%] - LeetPush

--- 1514-Path-with-Maximum-Probability.py
+++ 1514-Path-with-Maximum-Probability.py
@@ -1,22 +1,18 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        graph = defaultdict(list)
+        adj = defaultdict(list)
+
         for i in range(len(edges)):
-            graph[edges[i][0]].append((edges[i][1], succProb[i]))
-            graph[edges[i][1]].append((edges[i][0], succProb[i]))
-
+            adj[edges[i][0]].append((edges[i][1], succProb[i]))
+            adj[edges[i][1]].append((edges[i][0], succProb[i]))
         seen = set()
-        minH = [[-1, start_node]]
-
-        while minH:
-            prod, node = heapq.heappop(minH)
+        h = [[-1, start_node]]
+        while h:
+            prob, node = heapq.heappop(h)
             if node == end_node:
-                return -prod
+                return -prob
             seen.add(node)
-            for nei, neiCost in graph[node]:
+            for nei, neiCost in adj[node]:
                 if nei not in seen:
-                    heapq.heappush(minH, [prod * neiCost, nei])
-
+                    heapq.heappush(h, [neiCost*prob, nei])
         return 0
-
-        
