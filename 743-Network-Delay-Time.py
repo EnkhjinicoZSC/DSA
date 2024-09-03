6ab5d2b1b98d9b2fac54171a2950b82030bc6a23
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-09-03 13:35:40 -0700

    [743] [Time Beats: 47.89%] [Memory Beats: 95.22%] - LeetPush

--- 743-Network-Delay-Time.py
+++ 743-Network-Delay-Time.py
@@ -1,25 +1,19 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        minH = [[0, k]]
-        visited = set()
         adj = {i: [] for i in range(1, n + 1)}
-        
-        # construct adj
         for u, v, w in times:
             adj[u].append([v, w])
 
-        # Use a variable to track the maximum cost directly
-        max_cost = 0
-        
-        while minH:
-            cost, node = heapq.heappop(minH)
-            if node in visited:
+        h = [[0, k]]
+        visit = set()
+        while h:
+            weight, node = heapq.heappop(h)
+            if node in visit:
                 continue
-            visited.add(node)
-            max_cost = max(max_cost, cost)
+            visit.add(node)
+            if len(visit) == n:
+                return weight
             for nei, wei in adj[node]:
-                if nei not in visited:
-                    heapq.heappush(minH, [wei + cost, nei])
-        
-        # If all nodes are visited, return the max cost, else return -1
-        return max_cost if len(visited) == n else -1
+                if nei not in visit:
+                    heapq.heappush(h, [(weight + wei), nei])
+        return -1
