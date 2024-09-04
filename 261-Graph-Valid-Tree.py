055b28c7471b361438a8fd3197f63490751f77c7
EnkhjinicoZHC <enkhjinico@gmail.com>
2024-09-03 21:45:03 -0700

    new graph problem

---
+++ 261-Graph-Valid-Tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visit = set()
+        def dfs(i, prev):
+            if i in visit:
+                return False
+            visit.add(i)
+            for j in adj[i]:
+                if j == prev: continue
+                if not dfs(j, i):
+                    return False
+            return True
+
+        return dfs(0, -1) and len(visit) == n
+
