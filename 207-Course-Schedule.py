af2825e62b2bbbbe6b345532bc40ab3ac87eb210
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-09-05 13:39:45 -0700

    [207] [Time Beats: 11.41%] [Memory Beats: 52.89%] - LeetPush

--- 207-Course-Schedule.py
+++ 207-Course-Schedule.py
@@ -1,24 +1,29 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courseMap = {i: [] for i in range(numCourses)}
+        adj = defaultdict(list)
+        visit = set()
 
         for crs, pre in prerequisites:
-            courseMap[crs].append(pre)
-
-        seen = set()
-
+            adj[crs].append(pre)
         def dfs(crs):
-            if crs in seen: return False
-            if courseMap[crs] == []: return True
-
-            seen.add(crs)
-
-            for nei in courseMap[crs]:
-                if not dfs(nei): return False
-            seen.remove(crs)
-            courseMap[crs] = []
+            if crs in visit:
+                return False
+            if adj[crs] == []: return True
+            visit.add(crs)
+            for child in adj[crs]:
+                if not dfs(child):
+                    return False
+            visit.remove(crs)
+            adj[crs] = []
             return True
-
+        
         for crs in range(numCourses):
             if not dfs(crs): return False
         return True
+
+
+
+
+
+            
+
