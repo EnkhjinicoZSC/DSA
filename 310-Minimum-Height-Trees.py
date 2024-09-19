38b3e5d7e31604b3fa9c32ccc567aa69ef86f6a0
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-09-19 15:16:21 -0700

    [310] [Time Beats: 43.93%] [Memory Beats: 47.30%] - LeetPush

---
+++ 310-Minimum-Height-Trees.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+
+        if n == 1:
+            return [0]
+            
+        adj = defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        leaves = deque([i for i in range(n) if len(adj[i]) == 1])
+
+        while n > 2:
+            n -= len(leaves)
+            new_leaves = deque()
+            for leaf in leaves:
+                neighbor = adj[leaf].pop()  
+                adj[neighbor].remove(leaf) 
+                if len(adj[neighbor]) == 1:  
+                    new_leaves.append(neighbor)
+            leaves = new_leaves
+
+        return list(leaves)
