d5562bd62a3cae29762b5a9d7a2dd1037bc8c741
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-09-21 15:46:53 -0700

    [206] [Time Beats: 5.50%] [Memory Beats: 85.13%] - LeetPush

--- 206-Reverse-Linked-List.py
+++ 206-Reverse-Linked-List.py
@@ -8,9 +8,8 @@ class Solution:
         curr = head
         prev = None
         while curr:
-            nxt = curr.next
+            next_node = curr.next
             curr.next = prev
             prev = curr
-            curr = nxt
+            curr = next_node
         return prev
-        
