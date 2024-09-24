945d0dfa2f0ce1de27c84915a502038564233a28
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-09-24 12:40:48 -0700

    [572] [Time Beats: 93.13%] [Memory Beats: 54.85%] - LeetPush

--- 572-Subtree-of-Another-Tree.py
+++ 572-Subtree-of-Another-Tree.py
@@ -8,13 +8,14 @@ class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not root: return False
         if not subRoot: return True
-        if self.isSameTree(root, subRoot):
+        if self.isSameTree(root, subRoot): 
             return True
-            
+
         left = self.isSubtree(root.left, subRoot)
         right = self.isSubtree(root.right, subRoot)
 
         return left or right
+
 
     def isSameTree(self, s, t):
         if not s and not t:
@@ -22,3 +23,4 @@ class Solution:
         if s and t and s.val == t.val:
             return self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
         return False
+
