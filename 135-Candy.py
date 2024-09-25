752f087f3f0a1432c0f947fca9e46ee8a21db493
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-09-25 10:52:30 -0700

    [135] [Time Beats: 35.27%] [Memory Beats: 83.40%] - LeetPush

--- 135-Candy.py
+++ 135-Candy.py
@@ -1,14 +1,12 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        candies = [1] * len(ratings)
+        res = [1] * len(ratings)
 
         for i in range(1, len(ratings)):
-            if ratings[i - 1] < ratings[i]:
-                candies[i] = candies[i - 1] + 1
-
+            if ratings[i] > ratings[i-1]:
+                res[i] = res[i-1] + 1
+        
         for i in range(len(ratings) - 2, -1, -1):
-            if ratings[i + 1] < ratings[i]:
-                candies[i] = max(candies[i+1] + 1, candies[i])
-
-        return sum(candies)
-
+            if ratings[i+1] < ratings[i]:
+                res[i] = max(res[i+1] + 1, res[i])
+        return sum(res)
