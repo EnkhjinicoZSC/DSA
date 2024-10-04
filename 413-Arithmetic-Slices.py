16844a3cd0a74a57d6c055f38adff40eac980eef
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-10-04 15:18:30 -0700

    [413] [Time Beats: 73.24%] [Memory Beats: 26.66%] - LeetPush

---
+++ 413-Arithmetic-Slices.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return 0
+
+        count, curr= 0, 0
+
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                curr += 1
+                count += curr
+            else:
+                curr = 0
+        return count
