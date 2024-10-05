9509d1d2be2715cf2b42f0c71cebd4ded3f834e9
Nico <93143490+EnkhjinicoZSC@users.noreply.github.com>
2024-10-04 17:15:07 -0700

    [1035] [Time Beats: 75.41%] [Memory Beats: 41.90%] - LeetPush

--- 1035-Uncrossed-Lines.py
+++ 1035-Uncrossed-Lines.py
@@ -1,13 +1,24 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         
-        @cache
-        def dp(i, j):
-            # base case
-            if i == len(nums1) or j == len(nums2):
-                return 0
-            if nums1[i] == nums2[j]:
-                return 1 + dp(i + 1, j +  1)
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    dp[i + 1][j + 1] = 1 + dp[i][j]
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+
+        return dp[len(nums1)][len(nums2)]
+
+        # @cache
+        # def dp(i, j):
+        #     # base case
+        #     if i == len(nums1) or j == len(nums2):
+        #         return 0
+        #     if nums1[i] == nums2[j]:
+        #         return 1 + dp(i + 1, j +  1)
             
-            return max(dp(i+1, j), dp(i, j+1))
-        return dp(0, 0)
+        #     return max(dp(i+1, j), dp(i, j+1))
+        # return dp(0, 0)
